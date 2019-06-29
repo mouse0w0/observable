24cb6068439b95199be4559e3cb7e316aa262d2d
Mouse <mouse0w0@qq.com>
2019-06-29 22:01:29 +0800

    Update collections

--- src/main/java/com/github/mouse0w0/observable/collection/ObservableQueue.java
+++ src/main/java/com/github/mouse0w0/observable/collection/ObservableQueue.java
@@ -1,5 +1,6 @@
 package com.github.mouse0w0.observable.collection;
 
+import java.util.Collections;
 import java.util.Queue;
 
 public interface ObservableQueue<E> extends Queue<E> {
@@ -7,4 +8,8 @@ public interface ObservableQueue<E> extends Queue<E> {
     void addChangeListener(QueueChangeListener<? super E> listener);
 
     void removeChangeListener(QueueChangeListener<? super E> listener);
+
+    default boolean addAll(E... elements) {
+        return Collections.addAll(this, elements);
+    }
 }
--- src/main/java/com/github/mouse0w0/observable/collection/ObservableSet.java
+++ src/main/java/com/github/mouse0w0/observable/collection/ObservableSet.java
@@ -1,5 +1,6 @@
 package com.github.mouse0w0.observable.collection;
 
+import java.util.Collections;
 import java.util.Set;
 
 public interface ObservableSet<E> extends Set<E> {
@@ -7,4 +8,8 @@ public interface ObservableSet<E> extends Set<E> {
     void addChangeListener(SetChangeListener<? super E> listener);
 
     void removeChangeListener(SetChangeListener<? super E> listener);
+
+    default boolean addAll(E... elements) {
+        return Collections.addAll(this, elements);
+    }
 }
