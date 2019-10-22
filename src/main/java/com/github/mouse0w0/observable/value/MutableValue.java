b10a10b4f7546918df16e7c62fa6ef42ecd5d5f5
Mouse <mouse0w0@qq.com>
2019-10-22 17:39:29 +0800

    Add bindBidirectional and unbindBidirectional to MutableValue

--- src/main/java/com/github/mouse0w0/observable/value/MutableValue.java
+++ src/main/java/com/github/mouse0w0/observable/value/MutableValue.java
@@ -1,8 +1,18 @@
 package com.github.mouse0w0.observable.value;
 
+import com.github.mouse0w0.observable.binding.Bindings;
+
 public interface MutableValue<T> extends ObservableValue<T> {
 
     void setValue(T value);
 
     ObservableValue<T> toImmutable();
+
+    default void bindBidirectional(MutableValue<T> other) {
+        Bindings.bind(this, other);
+    }
+
+    default void unbindBidirectional(MutableValue<T> other) {
+        Bindings.unbind(this, other);
+    }
 }
