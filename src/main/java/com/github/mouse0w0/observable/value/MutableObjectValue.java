b05301d88e5cf697ba04cfd9c4d0619014e7df5d
Mouse <mouse0w0@qq.com>
2019-10-23 00:15:33 +0800

    Fix toImmutable of MutableObjectValue

--- src/main/java/com/github/mouse0w0/observable/value/MutableObjectValue.java
+++ src/main/java/com/github/mouse0w0/observable/value/MutableObjectValue.java
@@ -3,4 +3,6 @@ package com.github.mouse0w0.observable.value;
 public interface MutableObjectValue<T> extends MutableValue<T> {
 
     void set(T value);
+
+    ObservableObjectValue<T> toImmutable();
 }
