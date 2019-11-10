7684e8c7b2c83bf2737b4b420345aec15703c90c
Mouse <mouse0w0@qq.com>
2019-11-10 23:15:08 +0800

    Fix MutableObjectValue not extends ObservableObjectValue

--- src/main/java/com/github/mouse0w0/observable/value/MutableObjectValue.java
+++ src/main/java/com/github/mouse0w0/observable/value/MutableObjectValue.java
@@ -1,6 +1,6 @@
 package com.github.mouse0w0.observable.value;
 
-public interface MutableObjectValue<T> extends MutableValue<T> {
+public interface MutableObjectValue<T> extends ObservableObjectValue<T>, MutableValue<T> {
 
     void set(T value);
 
