050e01805b248df099737ed88b2cc020b1a5a121
Mouse <mouse0w0@qq.com>
2021-11-18 19:33:33 +0800

    remove UnidirectionalBinding

--- src/main/java/com/github/mouse0w0/observable/value/MutableValue.java
+++ src/main/java/com/github/mouse0w0/observable/value/MutableValue.java
@@ -1,19 +1,10 @@
 package com.github.mouse0w0.observable.value;
 
 import com.github.mouse0w0.observable.binding.BidirectionalBinding;
-import com.github.mouse0w0.observable.binding.UnidirectionalBinding;
 
 public interface MutableValue<T> extends ObservableValue<T> {
 
     void setValue(T value);
-
-    default void bind(ObservableValue<T> other) {
-        UnidirectionalBinding.bind(this, other);
-    }
-
-    default void unbind(ObservableValue<T> other) {
-        UnidirectionalBinding.unbind(this, other);
-    }
 
     default void bindBidirectional(MutableValue<T> other) {
         BidirectionalBinding.bind(this, other);
