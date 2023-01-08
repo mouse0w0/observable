ee123d187bb2d2e94a982bb1b0d1caeaed8900d0
Mouse <mouse0w0@qq.com>
2023-01-08 22:33:44 +0800

    add empty collections

--- src/main/java/com/github/mouse0w0/observable/collection/ObservableCollections.java
+++ src/main/java/com/github/mouse0w0/observable/collection/ObservableCollections.java
@@ -3,7 +3,7 @@ package com.github.mouse0w0.observable.collection;
 import com.github.mouse0w0.observable.InvalidationListener;
 
 import java.util.*;
-import java.util.function.Supplier;
+import java.util.function.*;
 
 public final class ObservableCollections {
 
@@ -23,6 +23,12 @@ public final class ObservableCollections {
         return new UnmodifiableObservableList<>(list);
     }
 
+    private static final ObservableList EMPTY_OBSERVABLE_LIST = new EmptyObservableList();
+
+    public static <E> ObservableList<E> emptyObservableList() {
+        return EMPTY_OBSERVABLE_LIST;
+    }
+
     public static <E> ObservableSet<E> observableHashSet() {
         return observableSet(new HashSet<>());
     }
@@ -39,6 +45,12 @@ public final class ObservableCollections {
         return new UnmodifiableObservableSet<>(set);
     }
 
+    private static final ObservableSet EMPTY_OBSERVABLE_SET = new EmptyObservableSet();
+
+    public static <E> ObservableSet<E> emptyObservableSet() {
+        return EMPTY_OBSERVABLE_SET;
+    }
+
     public static <K, V> ObservableMap<K, V> observableHashMap() {
         return observableMap(new HashMap<>());
     }
@@ -53,6 +65,12 @@ public final class ObservableCollections {
 
     public static <K, V> ObservableMap<K, V> unmodifiableObservableMap(ObservableMap<K, V> map) {
         return new UnmodifiableObservableMap<>(map);
+    }
+
+    private static final ObservableMap EMPTY_OBSERVABLE_MAP = new EmptyObservableMap();
+
+    public static <K, V> ObservableMap<K, V> emptyObservableMap() {
+        return EMPTY_OBSERVABLE_MAP;
     }
 
     private static class UnmodifiableObservableList<E> extends AbstractList<E> implements ObservableList<E> {
@@ -259,6 +277,314 @@ public final class ObservableCollections {
         @Override
         public void removeListener(MapChangeListener<? super K, ? super V> listener) {
             map.removeListener(listener);
+        }
+    }
+
+    private static class EmptyObservableList<E> extends AbstractList<E> implements ObservableList<E> {
+
+        @Override
+        public void addListener(InvalidationListener listener) {
+
+        }
+
+        @Override
+        public void removeListener(InvalidationListener listener) {
+
+        }
+
+        @Override
+        public void addListener(ListChangeListener<? super E> listener) {
+
+        }
+
+        @Override
+        public void removeListener(ListChangeListener<? super E> listener) {
+
+        }
+
+        @Override
+        public E get(int index) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        @Override
+        public int size() {
+            return 0;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return true;
+        }
+
+        @Override
+        public boolean contains(Object o) {
+            return false;
+        }
+
+        @Override
+        public boolean containsAll(Collection<?> c) {
+            return c.isEmpty();
+        }
+
+        @Override
+        public Object[] toArray() {
+            return new Object[0];
+        }
+
+        @Override
+        public <T> T[] toArray(T[] a) {
+            if (a.length > 0)
+                a[0] = null;
+            return a;
+        }
+
+        @Override
+        public Iterator<E> iterator() {
+            return Collections.emptyIterator();
+        }
+
+        @Override
+        public ListIterator<E> listIterator() {
+            return Collections.emptyListIterator();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            return o instanceof List && ((List<?>) o).isEmpty();
+        }
+
+        @Override
+        public int hashCode() {
+            return 1;
+        }
+
+        @Override
+        public void sort(Comparator<? super E> c) {
+        }
+
+        @Override
+        public boolean removeIf(Predicate<? super E> filter) {
+            Objects.requireNonNull(filter);
+            return false;
+        }
+
+        @Override
+        public void replaceAll(UnaryOperator<E> operator) {
+            Objects.requireNonNull(operator);
+        }
+
+        @Override
+        public void forEach(Consumer<? super E> action) {
+            Objects.requireNonNull(action);
+        }
+
+        @Override
+        public Spliterator<E> spliterator() {
+            return Spliterators.emptySpliterator();
+        }
+    }
+
+    private static class EmptyObservableSet<E> extends AbstractSet<E> implements ObservableSet<E> {
+
+        @Override
+        public void addListener(InvalidationListener listener) {
+
+        }
+
+        @Override
+        public void removeListener(InvalidationListener listener) {
+
+        }
+
+        @Override
+        public void addListener(SetChangeListener<? super E> listener) {
+
+        }
+
+        @Override
+        public void removeListener(SetChangeListener<? super E> listener) {
+
+        }
+
+        @Override
+        public Iterator<E> iterator() {
+            return Collections.emptyIterator();
+        }
+
+        @Override
+        public int size() {
+            return 0;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return true;
+        }
+
+        @Override
+        public boolean contains(Object o) {
+            return false;
+        }
+
+        @Override
+        public boolean containsAll(Collection<?> c) {
+            return c.isEmpty();
+        }
+
+        @Override
+        public Object[] toArray() {
+            return new Object[0];
+        }
+
+        @Override
+        public <T> T[] toArray(T[] a) {
+            if (a.length > 0)
+                a[0] = null;
+            return a;
+        }
+
+        @Override
+        public boolean removeIf(Predicate<? super E> filter) {
+            Objects.requireNonNull(filter);
+            return false;
+        }
+
+        @Override
+        public void forEach(Consumer<? super E> action) {
+            Objects.requireNonNull(action);
+        }
+
+        @Override
+        public Spliterator<E> spliterator() {
+            return Spliterators.emptySpliterator();
+        }
+    }
+
+    private static class EmptyObservableMap<K, V> extends AbstractMap<K, V> implements ObservableMap<K, V> {
+
+        @Override
+        public void addListener(InvalidationListener listener) {
+
+        }
+
+        @Override
+        public void removeListener(InvalidationListener listener) {
+
+        }
+
+        @Override
+        public void addListener(MapChangeListener<? super K, ? super V> listener) {
+
+        }
+
+        @Override
+        public void removeListener(MapChangeListener<? super K, ? super V> listener) {
+
+        }
+
+        @Override
+        public int size() {
+            return 0;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return true;
+        }
+
+        @Override
+        public boolean containsKey(Object key) {
+            return false;
+        }
+
+        @Override
+        public boolean containsValue(Object value) {
+            return false;
+        }
+
+        @Override
+        public Set<K> keySet() {
+            return emptyObservableSet();
+        }
+
+        @Override
+        public Collection<V> values() {
+            return emptyObservableSet();
+        }
+
+        @Override
+        public Set<Entry<K, V>> entrySet() {
+            return emptyObservableSet();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            return o instanceof Map && ((Map<?, ?>) o).isEmpty();
+        }
+
+        @Override
+        public int hashCode() {
+            return 0;
+        }
+
+        @Override
+        public V getOrDefault(Object k, V defaultValue) {
+            return defaultValue;
+        }
+
+        @Override
+        public void forEach(BiConsumer<? super K, ? super V> action) {
+            Objects.requireNonNull(action);
+        }
+
+        @Override
+        public void replaceAll(BiFunction<? super K, ? super V, ? extends V> function) {
+            Objects.requireNonNull(function);
+        }
+
+        @Override
+        public V putIfAbsent(K key, V value) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public boolean remove(Object key, Object value) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public boolean replace(K key, V oldValue, V newValue) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public V replace(K key, V value) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public V computeIfAbsent(K key,
+                                 Function<? super K, ? extends V> mappingFunction) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public V computeIfPresent(K key,
+                                  BiFunction<? super K, ? super V, ? extends V> remappingFunction) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public V compute(K key,
+                         BiFunction<? super K, ? super V, ? extends V> remappingFunction) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public V merge(K key, V value,
+                       BiFunction<? super V, ? super V, ? extends V> remappingFunction) {
+            throw new UnsupportedOperationException();
         }
     }
 
